e49bcf013b923815374f2e1721f653c6c5284f1b
John <146442701+joroho73@users.noreply.github.com>
2024-02-29 08:00:30 +0000

    added address_lookup

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import company
+from .views import company, address_lookup
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("company/", company, name="company"),
+    path('address-lookup/', address_lookup, name='address_lookup'),
 ]
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+import requests
 
 
 def company(request):
     return render(request=request, template_name="company_signup.html")
+
+def address_lookup(request):
+    return render(request=request, template_name="address_lookup.html")
