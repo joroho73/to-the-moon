1392924dd5b1aff3d2e7fb36a75d97d1ab99e92e
John <146442701+joroho73@users.noreply.github.com>
2024-02-29 22:33:49 +0000

    user journey: enter postcode > view addresses > select address > view companies associated with a query based on the address data

--- mysite/app/external_api.py
+++ mysite/app/external_api.py
@@ -1,21 +1,26 @@
-# external_api.py
-
 import requests
+import base64
+from requests.auth import HTTPBasicAuth
 
 
-COMPANIES_HOUSE_KEY = '1ae6053d-cfe4-49eb-9fae-2d8834c634ac'
+# Note, in production this data would be encrypted
+COMPANIES_HOUSE_KEY = '1ae6053d-cfe4-49eb-9fae-2d8834c634ac' # not working in live
+MY_KEY = '8b5ef71d-4a50-402f-a372-f7a3a4b12d21' # new key generated
 TARIFFSCANNER_KEY = 'FtNdbL*B6$9ZNq'
 
 class AddressLookupAPI:
-    BASE_URL = "https://api.ideal-postcodes.co.uk/v1/autocomplete/addresses"
+    POSTCODES_BASE_URL = "https://api.ideal-postcodes.co.uk/v1/autocomplete/addresses"
     POSTCODES_KEY = "ak_lt21qifpaE8hyWiA9lsLS9t1vPmr4"
 
     def lookup(self, postcode):
-        api_url = f"{self.BASE_URL}?query={postcode}&api_key={self.POSTCODES_KEY}"
+        # Note, using the key as a querystring parameter is not secure, instead we need to use the 
+        # basic Headers authentication but I couldn't get tis working in the postcodes lookup.
+        api_url = f"{self.POSTCODES_BASE_URL}?query={postcode}&api_key={self.POSTCODES_KEY}"
 
         try:
             # Make the API call
             response = requests.get(api_url)
+            print(response.status_code)
             
             # Check if the request was successful (status code 200)
             if response.status_code == 200:
@@ -29,3 +34,35 @@ class AddressLookupAPI:
             # Handle exceptions
             print(e)
             return None
+        
+
+class CompanyLookupAPI:
+    COMPANIES_HOUSE_BASE_URL = "https://api.company-information.service.gov.uk/search/companies"
+    COMPANIES_HOUSE_KEY = "a732b3cb-1c69-4f03-8573-a0e55b93f937"  
+        
+    def lookup_with_query(self, query):
+        api_url = f"{self.COMPANIES_HOUSE_BASE_URL}?q={query}"
+        print(api_url)
+
+        # Auth variables
+        username = self.COMPANIES_HOUSE_KEY
+        password = ""  
+
+        try:
+            # Make a GET request to the API with basic authentication
+            response = requests.get(api_url, auth=(username, password))
+            
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                # Parse the JSON response
+                data = response.json()
+                return data
+            else:
+                # If the request was unsuccessful, return None
+                return None
+        except Exception as e:
+            # Handle exceptions
+            print(e)
+            return None
+
+    
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
-from .external_api import AddressLookupAPI  # Import the AddressLookup class
+from django.http import JsonResponse
+from .external_api import AddressLookupAPI, CompanyLookupAPI  # Import the API Lookup classes
 
 
 def company(request):
@@ -11,6 +12,7 @@ class CompanySignupView(View): #Inherit from View
     def get(self, request):
         return render(request, 'company_signup.html')
 
+# Address look up 
 class AddressLookupView(View):
     def get(self, request):
         postcode = request.GET.get('postcode')
@@ -20,13 +22,14 @@ class AddressLookupView(View):
         
         # Call the lookup method of AddressLookupAPI to fetch data from the API
         response_data = address_lookup_api.lookup(postcode)
+        print(response_data)
     
         if response_data:
             suggestions = self.process_addresses(response_data)
             print(suggestions)
             return render(request, 'address_lookup.html', {'postcode': postcode, 'suggestions': suggestions})
         else:
-            return render(request, 'error_template.html', {'error': 'Failed to fetch data from API'})
+            return JsonResponse({'error': 'Failed to fetch data from API'}, status=500)
         
 
     def process_addresses(self, response_data):
@@ -37,22 +40,36 @@ class AddressLookupView(View):
         except (IndexError, KeyError):
             return []  # Return an empty list if there's an error accessing the data
 
+
+# Company look up
 class CompanyLookupView(View):
     def get(self, request):
         address = request.GET.get('address')
         address_id = request.GET.get('address_id')
-        
-        # Perform any necessary processing based on the address and address ID
-        
         return render(request, 'company_lookup.html', {'address': address, 'address_id': address_id})
     
     def post(self, request):
-        # Handle POST request to process submitted data
+        # Handle POST request to process submitted adress data
         print("CompanyLookupView POST")
         address = request.POST.get('selected_address')
         address_id = request.POST.get('address_id')
+        postcode = request.POST.get('postcode')
+
+        # Instantiate the CompanyLookupAPI class
+        companies_lookup_api = CompanyLookupAPI()
         
-        # Perform any necessary processing based on the address and address ID
+        # Call the lookup method of AddressLookupAPI to fetch data from the API
+        # this version uses the postcode specified because it seems to be a little more accurate 
+        # than using the address.
+        response_json = companies_lookup_api.lookup_with_query(postcode)
+        response_json_by_address = companies_lookup_api.lookup_with_query(address)
+        #print(response_json_by_address)
+
+        if response_json:
+            return render(request, 'company_lookup.html', {'address_id' : address_id, 'address' : address, 'postcode': postcode, 'response_data': response_json, 'response_data_by_address':response_json_by_address })
+        else:
+            return render(request, 'error_template.html', {'error': 'Failed to fetch data from API'})
+
         
-        return render(request, 'company_lookup.html', {'address': address, 'address_id': address_id})
-        pass
+        return render(request, 'company_lookup.html', {'address': address, 'address_id': address_id, 'postcode' : postcode, 'response_data': response_data })
+        
